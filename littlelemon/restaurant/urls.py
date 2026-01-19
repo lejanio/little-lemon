2b12275224738d03c65b15e0b642ca5311a380cb
lejanio <janis.leonciks@gmail.com>
2026-01-19 22:12:58 +0200

    Added index template and static file directory

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,5 +1,7 @@
-from
+from django.urls import path
+from .views import sayHello, index
 
 urlpatterns = [
-    
+    path('', index, name='index'),
+    path('hello/', sayHello, name='sayHello'),
 ]
